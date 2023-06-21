1504d512852ffc8640a8c4cbc7c655651a827a14
jarao <joan.adria.rao@jarao.org>
2023-06-21 16:17:39 +0200

    Added docstring to each function
    Signed-off-by: jarao <joan.adria.rao@jarao.org>

--- src/osadp.h
+++ src/osadp.h
@@ -48,16 +48,61 @@ typedef struct{
 
 }Satellite;
 
+/**
+* Triggers the execution of the OSADP protocol. This fuction is the main function
+* of the protocol execution. It starts the algorithm. 
+*/
 void start_osadp(Satellite sat, int port);
+
+/**
+* Transfers a PUBLISH message through a socket. It fills the different fields of the 
+* packet and trasnfer it through a UDP socket. 
+*/
 void send_publish(int sock, Satellite sat);
+
+/**
+* Retrieves the packet from the socket. It stores the received packet into a structure,
+* and also evaluates if the packet follows the OSADP protocol. 
+*/
 int check_received_packet(int sock, bool *cont, Satellite sat);
+
+/**
+* Propagates the received PUBLISH packet. After modifying the corresponding fields of
+* the packet, it sends it through a UDP socket.
+*/
 void forward_publish(int sock, Satellite sat);
+
+/**
+* Provides the system time in UTC.
+*/
 long get_current_time();
+
+/**
+* Cleans the structure of the packet. The packets are stored in the same structure
+* to recycle memory. In this case, this function cleans this structure and ensures
+* that no old values may interfere.
+*/
 void reset_rx_packet();
 
+/**
+* Insert the item at the beginning of the service table.
+*/
 void insertFirst(int key, int data);
+
+/**
+* Delete the first item of the service table.
+*/
 struct node* deleteFirst();
+
+/**
+* Find the service associated to the key
+*/
 struct node* find(int key);
+
+/**
+* Prints statistics and configuration of the OSDAP. This print is currently done 
+* through the stdout.
+*/
 void printList();
 
 #endif
